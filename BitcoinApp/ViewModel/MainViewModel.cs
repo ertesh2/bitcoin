48cf73d5039c46cf306e65930d54122caca89ef4
Maciej Andrejczuk <maandrej@microsoft.com>
2015-10-23 17:42:44 -0700

    Prototype of getting price from rest service.

--- BitcoinApp/ViewModel/MainViewModel.cs
+++ BitcoinApp/ViewModel/MainViewModel.cs
@@ -1,5 +1,11 @@
 ﻿using GalaSoft.MvvmLight;
 using System.ComponentModel;
+using System;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Data.Json;
 
 namespace BitcoinApp.ViewModel
 {
@@ -7,7 +13,40 @@ namespace BitcoinApp.ViewModel
     {
         public MainViewModel()
         {
-            _price = 1.5;
+            // TODO: Add dispatcher
+            //GetPrice().ContinueWith(t => Price = t.Result);
+        }
+
+        private async Task<double> GetPrice()
+        {
+            //rest sharp?
+
+            string requestUrl = "http://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+            using (HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception("Unexpected response: " + response.StatusCode);
+                }
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    try
+                    {
+                        string data = reader.ReadToEnd();
+                        JsonValue jsonValue = JsonValue.Parse(data);
+                        var time = jsonValue.GetObject().GetNamedValue("time");
+                        var bpi = jsonValue.GetObject().GetNamedValue("bpi");
+                        var usd = bpi.GetObject().GetNamedValue("USD");
+                        var rate = usd.GetObject().GetNamedString("rate");
+                        return Double.Parse(rate);
+                    }
+                    catch (Exception e)
+                    {
+                        return 0.0;
+                    }
+                }
+            }
         }
 
         private double _price;
